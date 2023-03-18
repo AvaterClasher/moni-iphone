30d4f06b3511c5d02003361d29c3023d6a7fcafa
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2023-03-18 12:09:50 +0530

    modified:   src/App.js 	modified:   src/sections/PhoneModel.js 	modified:   src/sections/Quote.js

--- src/sections/PhoneModel.js
+++ src/sections/PhoneModel.js
@@ -1,28 +1,31 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
-import Model from "../components/Scene"
-
+import React, { Suspense } from "react";
+import styled from "styled-components";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Environment } from "@react-three/drei";
+import Model from "../components/Scene";
 
 const Container = styled.div`
-width: 100vw;
-height: 100vh;
-position: fixed;
-top: 0;
-z-index: 1;
-background-color:transparent;
-`
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  z-index: 1;
+  background-color: transparent;
+`;
 const PhoneModel = () => {
   return (
     <Container>
-        <Canvas>
-        <ambientLight intensity={1.25}/>
-            <Model/>
-            <OrbitControls/>
-        </Canvas>
+      <Canvas camera={{ fov: 20 }}>
+        <ambientLight intensity={2.25} />
+        <directionalLight intensity={0.4} />
+        <Suspense fallback={null}> 
+          <Model scale={1.77} />
+        </Suspense>
+        <Environment preset="night" />
+        {/* <OrbitControls/> */}
+      </Canvas>
     </Container>
-  )
-}
+  );
+};
 
-export default PhoneModel
+export default PhoneModel;
